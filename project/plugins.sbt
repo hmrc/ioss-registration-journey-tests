05fcaf2c2b43d88da65a58e2206faf93d360e01e
susan-badel <57998768+susan-badel@users.noreply.github.com>
2024-02-12 18:22:42 +0000

    VEIOSS-532 Migrated to new UI test tooling (#52)
    * VEIOSS-532 Migrated to new UI test tooling
    
    * added db cleardown back in

--- project/plugins.sbt
+++ project/plugins.sbt
@@ -6,3 +6,5 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.20.0")
 
 addSbtPlugin("org.scalameta" % "sbt-scalafmt" % "2.4.0")
+
+addSbtPlugin("uk.gov.hmrc" % "sbt-test-report" % "0.22.0")
--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Hooks.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Hooks.scala
@@ -16,23 +16,18 @@
 
 package uk.gov.hmrc.test.ui.cucumber.stepdefs
 
-import io.cucumber.scala.{EN, ScalaDsl, Scenario}
-import org.openqa.selenium.{OutputType, TakesScreenshot}
+import io.cucumber.scala.{EN, ScalaDsl}
+import uk.gov.hmrc.selenium.webdriver.{Browser, Driver}
 import uk.gov.hmrc.test.ui.cucumber.utils.MongoConnection
-import uk.gov.hmrc.test.ui.driver.BrowserDriver
 
-class Hooks extends ScalaDsl with EN with BrowserDriver {
-  After { scenario: Scenario =>
-    if (scenario.isFailed) {
-      val screenshotName = scenario.getName.replaceAll(" ", "_")
-      val screenshot     = driver.asInstanceOf[TakesScreenshot].getScreenshotAs(OutputType.BYTES)
-      scenario.attach(screenshot, "image/png", screenshotName)
-    }
+object Hooks extends ScalaDsl with EN with Browser {
+  Before {
+    startBrowser()
+    MongoConnection.dropSavedAnswers()
   }
 
-  Before {
-    driver.manage().deleteAllCookies()
-    MongoConnection.dropSavedAnswers()
+  After {
+    quitBrowser()
   }
 
 }
