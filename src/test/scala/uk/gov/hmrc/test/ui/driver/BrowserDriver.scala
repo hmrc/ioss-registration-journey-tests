896e09602e5c7a6aa7c1a164a42301aa8a4b70a7
Susan Badel <susan.badel@opencastsoftware.com>
2024-02-12 17:39:45 +0000

    VEIOSS-532 Migrated to new UI test tooling

--- src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/driver/BrowserDriver.scala
@@ -16,14 +16,12 @@
 
 package uk.gov.hmrc.test.ui.driver
 
-import com.typesafe.scalalogging.LazyLogging
-import org.openqa.selenium.WebDriver
-import uk.gov.hmrc.webdriver.SingletonDriver
 
-trait BrowserDriver extends LazyLogging {
-  logger.info(
-    s"Instantiating Browser: ${sys.props.getOrElse("browser", "'browser' System property not set. This is required")}"
-  )
+import org.openqa.selenium.remote.RemoteWebDriver
+import uk.gov.hmrc.selenium.webdriver.Driver
 
-  implicit lazy val driver: WebDriver = SingletonDriver.getInstance()
+trait BrowserDriver {
+
+  implicit def driver: RemoteWebDriver = Driver.instance
+
 }
