97185328de45b1efd397e7bc2c3cd6f54d45680e
susan-badel <57998768+susan-badel@users.noreply.github.com>
2024-03-11 14:06:43 +0000

    VEIOSS-509 rejoin core validation scenarios (#55)
    * VEIOSS-509 rejoin core validation scenarios
    
    * more wip for quarantined rejoin scenarios
    
    * VEIOSS-509 updated urls for fixed establishment vrn already registered
    
    * VEIOSS-509 - updated steps for FEs
    
    * VEIOSS-509 update country code for active scheme

--- src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Hooks.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/cucumber/stepdefs/Hooks.scala
@@ -17,7 +17,7 @@
 package uk.gov.hmrc.test.ui.cucumber.stepdefs
 
 import io.cucumber.scala.{EN, ScalaDsl}
-import uk.gov.hmrc.selenium.webdriver.{Browser, Driver}
+import uk.gov.hmrc.selenium.webdriver.Browser
 import uk.gov.hmrc.test.ui.cucumber.utils.MongoConnection
 
 object Hooks extends ScalaDsl with EN with Browser {
