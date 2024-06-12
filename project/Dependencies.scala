823975edcd36125bcb17e122ba7907ca4667f872
susan-badel <57998768+susan-badel@users.noreply.github.com>
2024-06-12 10:47:35 +0100

    updated UI runner and other plugins/dependencies (#69)
    * updated UI runner and other plugins/dependencies
    
    * another version update

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,8 +4,8 @@ object Dependencies {
 
   val test: Seq[ModuleID] = Seq(
     "com.typesafe"         % "config"             % "1.4.2"    % Test,
-    "com.vladsch.flexmark" % "flexmark-all"       % "0.62.2"   % Test,
-    "org.scalatest"       %% "scalatest"          % "3.2.17"   % Test,
+    "com.vladsch.flexmark" % "flexmark-all"       % "0.64.8"   % Test,
+    "org.scalatest"       %% "scalatest"          % "3.2.18"   % Test,
     "org.scalatestplus"   %% "selenium-4-12"      % "3.2.17.0" % Test,
     "io.cucumber"         %% "cucumber-scala"     % "8.20.0"   % Test,
     "io.cucumber"          % "cucumber-junit"     % "7.15.0"   % Test,
@@ -13,7 +13,7 @@ object Dependencies {
     "com.novocode"         % "junit-interface"    % "0.11"     % Test,
     "org.mongodb.scala"   %% "mongo-scala-driver" % "4.7.1"    % Test,
     "org.slf4j"            % "slf4j-simple"       % "1.7.36"   % Test,
-    "uk.gov.hmrc"         %% "ui-test-runner"     % "0.17.0"   % Test
+    "uk.gov.hmrc"         %% "ui-test-runner"     % "0.30.0"   % Test
   )
 
 }
