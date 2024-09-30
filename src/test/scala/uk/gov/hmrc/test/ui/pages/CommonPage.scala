26bb9d1c31893ed6e65110b0b72a8f48d06c6c8a
Susan Badel <susan.badel@opencastsoftware.com>
2024-09-30 13:55:50 +0100

    VEIOSS-689 updated tests following javascript changes

--- src/test/scala/uk/gov/hmrc/test/ui/pages/CommonPage.scala
+++ src/test/scala/uk/gov/hmrc/test/ui/pages/CommonPage.scala
@@ -43,6 +43,9 @@ object CommonPage extends BasePage {
   def clickContinue(): Unit =
     driver.findElement(By.id("continue")).click()
 
+  def clickSubmit(): Unit =
+    driver.findElement(By.id("submit")).click()
+
   def enterData(data: String, inputId: String = "value"): Unit = {
     driver.findElement(By.id(inputId)).sendKeys(data)
     CommonPage.clickContinue()
